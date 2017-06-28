7e52e8bf0d6faec96d67e1800409867df74b5b43
Pia Sinzig <pia.sinzig@webbite.de>
2017-06-28 16:39:32 +0200

    Bis Video 10

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,31 +11,51 @@
 |
 */
 
-Route::get('/', function () {
+//use App\Task;
+
+/*Route::get('/', function () {
 	
 	$tasks = DB::table('tasks')->get();
 	
     return view('welcome', compact('tasks'));
-});
+});*/
+
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/{post}', 'PostController@show');
+
+//controller => PostsController
+//Eloquent model => Post
+//migration => create_posts_table
+
+//Route::get('/tasks', 'TasksController@index');
+
+//Route::get('/tasks/{task}', 'TasksController@show');
 
 
-Route::get('/tasks', function () {
 
-	$tasks = DB::table('tasks')->latest()->get();
+
+/*Route::get('/tasks', function () {
+
+	//$tasks = DB::table('tasks')->latest()->get();
+	
+	$tasks = Task::all();
 
 	return view('tasks.index', compact('tasks'));
-});
+});*/
 
 
-Route::get('/tasks/{task}', function ($id) {
+/*Route::get('/tasks/{task}', function ($id) {
 
 	//dd($id);
 	
-	$task = DB::table('tasks')->find($id);
+	//$task = DB::table('tasks')->find($id);
+	
+	$task = Task::find($id);
 	
 	//dd($task);
 	
 	//$tasks = DB::table('tasks')->get();
 
 	return view('tasks.show', compact('task'));
-});
+});*/
